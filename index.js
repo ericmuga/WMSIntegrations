b874ed254cec977eb7d4b0487dc32227b5bdc775
Eric Muga <eric.muga@gmail.com>
2024-12-19 10:59:00 +0300

    allowed breakingQueue

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ app.get('/fetch-production-orders', async (req, res) => {
      const { date, item, production_order_no } = req.query;
      let beheadingData= await consumeBeheadingData();
     //  let carcassSales=await consumeCarcassSales(); 
-    //  let breakingData= await consumeBreakingData();
+     let breakingData= await consumeBreakingData();
     //  let deboningData= await consumeDeboningData();
     //  let mincingFromButchery= await consume1570_2055();
     //   let choppingData=await consumechoppingData();
@@ -92,7 +92,7 @@ app.get('/fetch-production-orders', async (req, res) => {
 
                                                     beheadingData,
                                                     // carcassSales,
-                                                    // breakingData,
+                                                    breakingData,
                                                     // deboningData,
                                                     // mincingFromButchery,
                                                     // choppingData, 
