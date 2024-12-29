daca9ca5b83e7557a24c2274bf4b316989aaff58
Eric Muga <eric.muga@gmail.com>
2024-12-29 11:40:49 +0300

    after test with main dispatch printer

--- Services/printerService.js
+++ Services/printerService.js
@@ -88,12 +88,12 @@ const createPDF = async (data, pdfDirPath, itemNo, part, lines) => {
     const fileName = `${itemNo}_${part}.pdf`;
     const filePath = path.join(pdfDirPath, fileName);
 
-    const doc = new jsPDF('p', 'mm', 'a4'); // A4 size page
-    // const doc = new jsPDF({
-    //     orientation: 'p',
-    //     unit: 'mm',
-    //     format: [215, 285], // Width: 215mm, Height: 285mm
-    // });
+    // const doc = new jsPDF('p', 'mm', 'a4'); // A4 size page
+    const doc = new jsPDF({
+        orientation: 'p',
+        unit: 'mm',
+        format: [215, 285], // Width: 215mm, Height: 285mm
+    });
 
     doc.setFont("helvetica", "bold");
 
@@ -121,111 +121,109 @@ const createPDF = async (data, pdfDirPath, itemNo, part, lines) => {
 
     //switch between company printers
    
-    doc.setFont("helvetica", "bold");
-    doc.setFontSize(20);
-    // Drawing the header at slightly offset position of x to simulate a bolder text
-    doc.text(`DISPATCH Packing List ${part}        OF ${partsText}`, 105, 20, { align: 'center' });
-    doc.text(`DISPATCH Packing List ${part}        OF ${partsText}`, 105 + 0.2, 20, { align: 'center' });
-    doc.setFontSize(14);
+    // Initial line positions and spacing
+let y = 10; // Starting Y position
+const originalLineSpacing = 8; // Current line spacing
+const adjustedLineSpacing = originalLineSpacing * 0.8; // Reduce spacing to 0.8 times
 
-    doc.text(`${config.parkingListPrefix}${data.order_no}`, 25, 30)
+doc.setFont("helvetica", "bold");
+doc.setFontSize(16);
 
-    doc.text(`${data.ending_date} ${data.ending_time}`, 200, 30, { align: 'right' })
+// Drawing the header
+doc.text(`DISPATCH Packing List ${part}        OF ${partsText}`, 105,y , { align: 'center' });
+doc.text(`DISPATCH Packing List ${part}        OF ${partsText}`, 105 + 0.2, y, { align: 'center' });
+doc.setFontSize(12);
+y += adjustedLineSpacing;
 
-    doc.text(`${data.ending_date} ${data.ending_time}`, 180, 30, { align: 'right' })
 
-    // ----------------Line----------------
-    doc.text('Order Date:', 0, 38)
-    doc.text(data.shp_date, 42, 38)
+doc.text(`${config.parkingListPrefix}${data.order_no}`, 25, y);
+doc.text(`${data.ending_date} ${data.ending_time}`, 200, y, { align: 'right' });
+y += adjustedLineSpacing;
 
-    doc.text('Sell To Address:', 100, 38)
-    doc.text(data.shp_name, 140, 38)
+// ----------------Line----------------
+doc.text('Order Date:', 0, y);
+doc.text(data.shp_date, 42, y);
 
-    // ----------------Line----------------
-    doc.text('Order No:', 0, 46)
-    doc.text(`${config.orderPrefix}${data.order_no}`, 42, 46)
+doc.text('Sell To Address:', 100, y);
+doc.text(data.shp_name, 140, y);
+y += adjustedLineSpacing;
 
-    doc.text('Sales Person:', 100, 46)
-    doc.text(data.sp_code, 140, 46)
-    doc.text('Sales Person:', 100, 46)
-    doc.text(data.sp_code, 140, 46)
+// ----------------Line----------------
+doc.text('Order No:', 0, y);
+doc.text(`${config.orderPrefix}${data.order_no}`, 42, y);
 
-    // ----------------Line----------------
-    doc.text('Customer No:', 0, 54)
-    doc.text(data.customer_no, 42, 54)
+doc.text('Sales Person:', 100, y);
+doc.text(data.sp_code, 140, y);
+y += adjustedLineSpacing;
 
-    doc.text('', 100, 54)
-    doc.text(data.sp_name, 140, 54)
-    doc.text('', 100, 54)
-    doc.text(data.sp_name, 140, 54)
+// ----------------Line----------------
+doc.text('Customer No:', 0, y);
+doc.text(data.customer_no, 42, y);
 
-    // ----------------Line----------------
-    doc.text('Customer Name:', 0, 62)
-    doc.text(data.customer_name, 42, 62)
+doc.text('', 100, y);
+doc.text(data.sp_name, 140, y);
+y += adjustedLineSpacing;
 
-    doc.text('Delivery Date:', 100, 62)
-    doc.text(data.shp_date, 140, 62)
-    doc.text('Delivery Date:', 100, 62)
-    doc.text(data.shp_date, 140, 62)
+// ----------------Line----------------
+doc.text('Customer Name:', 0, y);
+doc.text(data.customer_name, 42, y);
 
-    // ----------------Line----------------
-    doc.text('External DocNo:', 0, 70)
-    doc.text(data.ext_doc_no, 42, 70)
+doc.text('Delivery Date:', 100, y);
+doc.text(data.shp_date, 140, y);
+y += adjustedLineSpacing;
 
-    doc.text('Ship To Name:', 100, 70)
-    doc.text(data.shp_name, 140, 70)
-    doc.text('Ship To Name:', 100, 70)
-    doc.text(data.shp_name, 140, 70)
+// ----------------Line----------------
+doc.text('External DocNo:', 0, y);
+doc.text(data.ext_doc_no, 42, y);
 
-    // ----------------Line----------------
-    doc.text('PDA Order:', 0, 78)
-    doc.text(data.pda ? 'Yes' : 'No', 42, 78)
+doc.text('Ship To Name:', 100, y);
+doc.text(data.shp_name, 140, y);
+y += adjustedLineSpacing;
 
-    doc.text('Cust Ref. No:', 100, 78)
-    doc.text(data.ext_doc_no, 140, 78)
-    doc.text('Cust Ref. No:', 100, 78)
-    doc.text(data.ext_doc_no, 140, 78)
+// ----------------Line----------------
+doc.text('PDA Order:', 0, y);
+doc.text(data.pda ? 'Yes' : 'No', 42, y);
 
-    // ----------------Line----------------
-    doc.setFontSize(10); //
-    doc.text('Order Receiver:', 0, 86)
-    doc.text(data.ended_by, 42, 86)
+doc.text('Cust Ref. No:', 100, y);
+doc.text(data.ext_doc_no, 140, y);
+y += adjustedLineSpacing;
 
-    // doc.setFontSize(20);
+// ----------------Line----------------
+doc.setFontSize(10);
+doc.text('Order Receiver:', 0, y);
+doc.text(data.ended_by, 42, y);
 
-    doc.text('External DocNo:', 100, 86)
-    doc.text(data.ext_doc_no, 140, 86)
+doc.text('External DocNo:', 100, y);
+doc.text(data.ext_doc_no, 140, y);
+y += adjustedLineSpacing;
 
-    // ----------------Line----------------
-    doc.text('Your Ref:', 0, 94)
-    doc.text('', 42, 94)
+// ----------------Line----------------
+doc.text('Your Ref:', 0, y);
+doc.text('', 42, y);
 
-    doc.text('District Group:', 100, 94)
-    doc.text('', 140, 94)
-    doc.text('District Group:', 100, 94)
-    doc.text('', 140, 94)
+doc.text('District Group:', 100, y);
+doc.text('', 140, y);
+y += adjustedLineSpacing;
 
-    // ----------------Line----------------
-    doc.text('Location:', 0, 102)
-    doc.text(data.route_code, 42, 102)
+// ----------------Line----------------
+doc.text('Location:', 0, y);
+doc.text(data.route_code, 42, y);
+
+doc.setFontSize(10);
+doc.text('Time Stamp:', 130, y);
+doc.text(Date.now().toString(), 200, y, { align: 'right' });
+y += adjustedLineSpacing;
+
+// Serial number
+const serial = await getSerialNumber('serial_number_counter').catch(error => {
+    console.error('Error fetching serial number:', error.message);
+    return 'DOC-00000000'; // Fallback value
+});
+
+doc.text('Serial No:', 130, y);
+doc.text(`${serial}`, 180, y, { align: 'right' });
 
     doc.setFontSize(12);
-    doc.text('Time Stamp:', 130, 102)
-    doc.text(Date.now().toString(), 200, 102, { align: 'right' })
-
-    const serial = await getSerialNumber('serial_number_counter')
-        .catch(error => {
-            console.error('Error fetching serial number:', error.message);
-
-            return 'DOC-00000000'; // Fallback value
-        });
-
-    console.log(serial)
-
-    doc.text('Serial No:', 130, 107)
-    doc.text(`${serial}`, 180, 107, { align: 'right' })
-
-    doc.setFontSize(14);
 
     const tableColumnNames = [
         'Item No.', 'Description', 'Cust. Specs', 'Unit of \nMeasure',
@@ -246,18 +244,18 @@ const createPDF = async (data, pdfDirPath, itemNo, part, lines) => {
     doc.autoTable({
     head: [tableColumnNames],
     body: tableData,
-    startY: 115,
+    startY: 80,
     startX: 2,
-    margin: { left: 2, top: 30, bottom: 80, right: 2 },
+    margin: { left: 2, top: 5, bottom: 30, right: 2 },
     columnStyles: {
-        0: { cellWidth: 40 * 0.8, fillColor: null, halign: 'left' }, // Reduce width by 20%
-        1: { cellWidth: 65 * 0.8, fillColor: null, halign: 'left' }, // Reduce width by 20%
-        2: { cellWidth: 20 * 0.8, fillColor: null, halign: 'left' }, // Reduce width by 20%
-        3: { cellWidth: 30 * 0.8, fillColor: null, halign: 'left' }, // Reduce width by 20%
-        4: { cellWidth: 15 * 0.8, fillColor: null, halign: 'left' }, // Reduce width by 20%
-        5: { cellWidth: 25 * 0.8, fillColor: null, halign: 'left' }, // Reduce width by 20%
-        6: { cellWidth: 20 * 0.8, fillColor: null, halign: 'left' }, // Reduce width by 20%
-        7: { cellWidth: 35 * 0.8, fillColor: null, halign: 'left' }, // Reduce width by 20%
+        0: { cellWidth: 40 * 0.9, fillColor: null, halign: 'left' }, // Reduce width by 20%
+        1: { cellWidth: 65 , fillColor: null, halign: 'left' }, // Reduce width by 20%
+        2: { cellWidth: 20 * 0.9, fillColor: null, halign: 'left' }, // Reduce width by 20%
+        3: { cellWidth: 30 * 0.9, fillColor: null, halign: 'left' }, // Reduce width by 20%
+        4: { cellWidth: 15 * 0.9, fillColor: null, halign: 'left' }, // Reduce width by 20%
+        5: { cellWidth: 25 * 0.9, fillColor: null, halign: 'left' }, // Reduce width by 20%
+        6: { cellWidth: 20 * 0.9, fillColor: null, halign: 'left' }, // Reduce width by 20%
+        7: { cellWidth: 35 * 0.9, fillColor: null, halign: 'left' }, // Reduce width by 20%
     },
     headStyles: {
         fillColor: null,
@@ -286,7 +284,7 @@ const createPDF = async (data, pdfDirPath, itemNo, part, lines) => {
             data.cell.styles.textColor = [0, 0, 0]
         },
         didDrawPage: (data) => {
-            const footerY = 230; // Fixed Y position for the footer
+            const footerY = 210; // Fixed Y position for the footer
 
             doc.setFontSize(12);
             doc.setFont("helvetica", "bold");
@@ -428,102 +426,102 @@ const loadConfig = (data) => {
 
 
 // await listPrinters();
-const sampleData=`{
-    "order_no": "BSO00031",
-    "ended_by": "AGILEBIZPAIVY.ESHIRERA",
-    "customer_no": "B25",
-    "customer_name": "Naivas Limited",
-    "shp_code": "B25_50_CM",
-    "shp_name": "NAIVAS - NEW BAMBURI",
-    "route_code": "B3535",
-    "sp_code": "B285",
-    "sp_name": "Mombasa Choice Meats",
-    "shp_date": "2024-12-20",
-    "assembler": "",
-    "checker": "",
-    "status": 4,
-    "pda": false,
-    "ending_time": "15:58:41.137",
-    "ending_date": "20240-20",
-    "ext_doc_no": "",
-    "company_flag": "Choice Meats",
-    "lines": [
-        {
-            "line_no": 10000,
-            "item_no": "BJ31015201",
-            "item_description": "Meaty Beef Sausages, 500gms",
-            "customer_spec": "",
-            "posting_group": "BF-SAUSAGE",
-            "part": "B",
-            "order_qty": 150,
-            "ass_qty": 0,
-            "exec_qty": 150,
-            "assembler": "",
-            "checker": "",
-            "barcode": "",
-            "qty_base": 150
-        },
-        {
-            "line_no": 20000,
-            "item_no": "BJ31015401",
-            "item_description": "Value Pack Beef Sausages 1kg",
-            "customer_spec": "",
-            "posting_group": "BF-SAUSAGE",
-            "part": "B",
-            "order_qty": 80,
-            "ass_qty": 0,
-            "exec_qty": 80,
-            "assembler": "",
-            "checker": "",
-            "barcode": "",
-            "qty_base": 80
-        },
-        {
-            "line_no": 30000,
-            "item_no": "BJ31015301",
-            "item_description": "Spicy Beef Sausages 500gms",
-            "customer_spec": "",
-            "posting_group": "BF-SAUSAGE",
-            "part": "B",
-            "order_qty": 0,
-            "ass_qty": 0,
-            "exec_qty": 0,
-            "assembler": "",
-            "checker": "",
-            "barcode": "",
-            "qty_base": 0
-        },
-        {
-            "line_no": 40000,
-            "item_no": "BJ31015101",
-            "item_description": "Beef Chipolatas 200gms",
-            "customer_spec": "",
-            "posting_group": "BF-SAUSAGE",
-            "part": "B",
-            "order_qty": 0,
-            "ass_qty": 0,
-            "exec_qty": 0,
-            "assembler": "",
-            "checker": "",
-            "barcode": "",
-            "qty_base": 0
-        },
-        {
-            "line_no": 50000,
-            "item_no": "BJ31100213",
-            "item_description": "Fresh Beef Burger 400gms",
-            "customer_spec": "",
-            "posting_group": "BF-FRSH BG",
-            "part": "D",
-            "order_qty": 0,
-            "ass_qty": 0,
-            "exec_qty": 0,
-            "assembler": "",
-            "checker": "",
-            "barcode": "",
-            "qty_base": 0
-        }
-    ]
-}`;
+// const sampleData=`{
+//     "order_no": "BSO00031",
+//     "ended_by": "AGILEBIZPAIVY.ESHIRERA",
+//     "customer_no": "B25",
+//     "customer_name": "Naivas Limited",
+//     "shp_code": "B25_50_CM",
+//     "shp_name": "NAIVAS - NEW BAMBURI",
+//     "route_code": "B3535",
+//     "sp_code": "B285",
+//     "sp_name": "Mombasa Choice Meats",
+//     "shp_date": "2024-12-20",
+//     "assembler": "",
+//     "checker": "",
+//     "status": 4,
+//     "pda": false,
+//     "ending_time": "15:58:41.137",
+//     "ending_date": "20240-20",
+//     "ext_doc_no": "",
+//     "company_flag": "Choice Meats",
+//     "lines": [
+//         {
+//             "line_no": 10000,
+//             "item_no": "BJ31015201",
+//             "item_description": "Meaty Beef Sausages, 500gms",
+//             "customer_spec": "",
+//             "posting_group": "BF-SAUSAGE",
+//             "part": "B",
+//             "order_qty": 150,
+//             "ass_qty": 0,
+//             "exec_qty": 150,
+//             "assembler": "",
+//             "checker": "",
+//             "barcode": "",
+//             "qty_base": 150
+//         },
+//         {
+//             "line_no": 20000,
+//             "item_no": "BJ31015401",
+//             "item_description": "Value Pack Beef Sausages 1kg",
+//             "customer_spec": "",
+//             "posting_group": "BF-SAUSAGE",
+//             "part": "B",
+//             "order_qty": 80,
+//             "ass_qty": 0,
+//             "exec_qty": 80,
+//             "assembler": "",
+//             "checker": "",
+//             "barcode": "",
+//             "qty_base": 80
+//         },
+//         {
+//             "line_no": 30000,
+//             "item_no": "BJ31015301",
+//             "item_description": "Spicy Beef Sausages 500gms",
+//             "customer_spec": "",
+//             "posting_group": "BF-SAUSAGE",
+//             "part": "B",
+//             "order_qty": 0,
+//             "ass_qty": 0,
+//             "exec_qty": 0,
+//             "assembler": "",
+//             "checker": "",
+//             "barcode": "",
+//             "qty_base": 0
+//         },
+//         {
+//             "line_no": 40000,
+//             "item_no": "BJ31015101",
+//             "item_description": "Beef Chipolatas 200gms",
+//             "customer_spec": "",
+//             "posting_group": "BF-SAUSAGE",
+//             "part": "B",
+//             "order_qty": 0,
+//             "ass_qty": 0,
+//             "exec_qty": 0,
+//             "assembler": "",
+//             "checker": "",
+//             "barcode": "",
+//             "qty_base": 0
+//         },
+//         {
+//             "line_no": 50000,
+//             "item_no": "BJ31100213",
+//             "item_description": "Fresh Beef Burger 400gms",
+//             "customer_spec": "",
+//             "posting_group": "BF-FRSH BG",
+//             "part": "D",
+//             "order_qty": 0,
+//             "ass_qty": 0,
+//             "exec_qty": 0,
+//             "assembler": "",
+//             "checker": "",
+//             "barcode": "",
+//             "qty_base": 0
+//         }
+//     ]
+// }`;
 
-initPrinting(JSON.parse(sampleData));
+// initPrinting(JSON.parse(sampleData));
