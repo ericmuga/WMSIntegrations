f972b01438e8e62980a5fd653c24a2ed8158c6c0
Eric Muga <eric.muga@gmail.com>
2025-04-08 12:38:13 +0300

    responding with 20 and acking

--- Services/Utils/queueManager.js
+++ Services/Utils/queueManager.js
@@ -120,7 +120,7 @@ export async function fetchProductionOrdersFromQueue(batchSize = 100) {
 
         const content = JSON.parse(msg.content.toString());
         orders.push(content);
-        // channel.ack(msg);
+        channel.ack(msg);
     }
 
     await channel.close();
