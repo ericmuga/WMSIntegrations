6293e229b5112d7bce483b8bb3f66b18ce1bea14
Eric Muga <eric.muga@gmail.com>
2025-04-21 20:34:32 +0300

    added myself

--- index.js
+++ index.js
@@ -229,6 +229,12 @@ app.post('/:user/print-delivery', async (req,res) => {
         break;
       }
 
+      case 'EMUGA': {
+        const response = await axios.post('http://100.100.4.57:3001/print-delivery', req.body);
+        logger.info('Response from external API:', response.data);
+        break;
+      }
+
       case 'AMWAI': {
         const response = await axios.post('http://100.100.4.56:3001/print-delivery', req.body);
         logger.info('Response from external API:', response.data);
