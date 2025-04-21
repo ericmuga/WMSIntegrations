246d6b429b43c481696227614defa006975dfe9b
Eric Muga <eric.muga@gmail.com>
2025-04-21 21:41:29 +0300

    changed printers for james

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ app.post('/:user/print-invoice', async (req, res) => {
         break;
       }
        case 'JMATHENGE': {
-        const response = await axios.post('http://100.100.4.57:3001/print-invoice', req.body);
+        const response = await axios.post('http://100.100.4.56:3001/print-invoice', req.body);
         logger.info('Response from external API:', response.data);
         break;
       }
