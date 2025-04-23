0ecfe8ddb0e5fd3de830c05313d45a2740d9026f
Ephantus Karanja <ephantusslogan@gmail.com>
2025-04-23 17:19:46 +0300

    added missing slaps

--- Services/Utils/publishProductionOrders.js
+++ Services/Utils/publishProductionOrders.js
@@ -167,6 +167,70 @@ export async function fetchAndPublishSlaughterData() {
     return rows.length;
 }
 
+export async function fetchAndPublishMissingSlapsData() {
+    const pool = await getPool('wms');
+
+    const result = await pool.request().query(`
+        SELECT * FROM [calibra].[dbo].[missing_slap_data] 
+        WHERE Published = 0 
+        AND [created_at] >= DATEADD(d, -2, DATEDIFF(d, 0, GETDATE()))
+    `);
+
+    const rows = result.recordset;
+
+    if (!rows || rows.length === 0) {
+        logger.info('No new missing data found to publish.');
+        return 0;
+    }
+
+    const connection = await getRabbitMQConnection();
+    const channel = await connection.createChannel();
+    const queueName = 'slaughter_line.bc'; // Replace this with your actual routing key
+    await channel.assertExchange('fcl.exchange.direct', 'direct', { durable: true });
+
+    for (const row of rows) {
+        const payload = JSON.stringify([{
+            id: row.id,
+            slapmark: row.slapmark,
+            receipt_no: '',
+            item_code: row.item_code,
+            vendor_no: '',
+            vendor_name: '',
+            actual_weight: row.actual_weight,
+            net_weight: row.net_weight,
+            settlement_weight: row.settlement_weight,
+            meat_percent: row.meat_percent,
+            classification_code: row.classification_code,
+            manual_weight: '',
+            user_id: row.user_id,
+            company_name: 'FCL',
+            timestamp: row.created_at,
+        }]);
+
+        await channel.publish(
+            'fcl.exchange.direct',
+            queueName,
+            Buffer.from(payload),
+            {
+                persistent: true,
+                contentType: 'application/json',
+            }
+        );
+    }
+
+    logger.info(`Published ${rows.length} missing data to RabbitMQ`);
+    await channel.close();
+
+    const slaughterDataIds = rows.map(row => row.id).join(',');
+    await pool.request().query(`
+        UPDATE [calibra].[dbo].[missing_slap_data] 
+        SET Published = 1 
+        WHERE id IN (${slaughterDataIds})
+    `);
+
+    return rows.length;
+}
+
 // Run once on launch
 (async () => {
     try {
@@ -175,6 +239,9 @@ export async function fetchAndPublishSlaughterData() {
 
         const slaughterCount = await fetchAndPublishSlaughterData();
         console.log(`Published ${slaughterCount} slaughter data to RabbitMQ`);
+
+        const missingSlapsCount = await fetchAndPublishMissingSlapsData();
+        console.log(`Published ${missingSlapsCount} missing slaps data to RabbitMQ`);
     } catch (error) {
         console.error(`Error publishing data: ${error.message}`);
     }
@@ -188,6 +255,9 @@ setInterval(async () => {
 
         const slaughterCount = await fetchAndPublishSlaughterData();
         console.log(`Published ${slaughterCount} slaughter data to RabbitMQ`);
+
+        const missingSlapsCount = await fetchAndPublishMissingSlapsData();
+        console.log(`Published ${missingSlapsCount} missing slaps data to RabbitMQ`);
     } catch (error) {
         console.error(`Error publishing data: ${error.message}`);
     }
