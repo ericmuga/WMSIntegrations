284134379d2e6e4f4792678767c74680aafb0e26
Eric Muga <eric.muga@gmail.com>
2025-04-25 09:51:29 +0300

    deos to IT printer

--- index.js
+++ index.js
@@ -197,8 +197,8 @@ app.post('/:user/print-invoice', async (req, res) => {
       case 'DWANZA': {
          //SEND AN INVOICE VIA EMAIL
 
-        // const response = await axios.post('http://100.100.4.54:3001/print-invoice', req.body);
-        // logger.info('Response from external API:', response.data);
+        const response = await axios.post('http://100.100.2.59:3001/print-invoice', req.body);
+        logger.info('Response from external API:', response.data);
         break;
       }
        
@@ -280,9 +280,9 @@ app.post('/:user/print-delivery', async (req,res) => {
 
         //SEND DELIVERY VIA EMAIL
         
-          // const response = await axios.post('http://100.100.2.39:3001/print-delivery', req.body);
-          // logger.info('Response from external API:', response.data);
-          // break;
+          const response = await axios.post('http://100.100.2.39:3001/print-delivery', req.body);
+          logger.info('Response from external API:', response.data);
+          break;
         }
 
 
